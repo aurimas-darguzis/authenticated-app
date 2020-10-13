693d1941e33cb0fa4446d3535d60f7732ee175b9
aurimasdarguzis <aurimasdarguzis@gmail.com>
2020-10-13 01:09:07 +0100

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import * as ROUTES from './constants/routes';
+import useAuthListener from './hooks/use-auth-listener';
+import { Dashboard, Signin, Signup } from './pages';
 
 function App() {
+  const { user } = useAuthListener();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Router>
+          <Switch>
+            <IsUserRedirect user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.SIGN_IN}>
+              <Signin />
+            </IsUserRedirect>
+            <IsUserRedirect user={user} loggedInPath={ROUTES.DASHBOARD} path={ROUTES.SIGN_UP}>
+              <Signup />
+            </IsUserRedirect>
+            <ProtectedRoute user={user} path={ROUTES.DASHBOARD}>
+              <Dashboard />
+            </ProtectedRoute>
+          </Switch>
+        </Router>
       </header>
     </div>
   );
