3a4948028548930e41ecaadca1f52a66d4962d1f
aurimas-darguzis <aurimasdarguzis@gmail.com>
2020-12-09 21:59:49 +0000

    implement forgot password UI

--- src/pages/forgot-password/index.js
+++ src/pages/forgot-password/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import * as ROUTES from '../../constants/routes';
 import { Link } from 'react-router-dom';
+import './forgot-password.scss';
 
 export default function ForgtoPassword() {
   return (
-    <div>
-      TODO: implement this page
-      <Link to={ROUTES.HOME}>Back</Link>
-    </div>
+    <>
+      <header>
+        <h1>Forgot your password?</h1>
+        <h2>We'll send you an email with instructions</h2>
+      </header>
+      <form>
+        <div className="input-wrapper">
+          <div className="input-area">
+            <input
+              type="text"
+              name="handle"
+              placeholder="Username / email"
+              id="handle"
+              maxLength={256}
+              required
+              autoCapitalize="none"
+              autocomplete="username"
+              autoFocus
+            />
+            <label for="handle" className="input-label">
+              Username / email
+            </label>
+            <div className="input-underline"></div>
+          </div>
+          <div className="extra-info"></div>
+        </div>
+        <button>Reset password</button>
+      </form>
+    </>
   );
 }
