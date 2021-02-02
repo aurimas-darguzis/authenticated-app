b582c4916d65fc78c6288482a48ad4c8dda6b540
aurimas-darguzis <aurimasdarguzis@gmail.com>
2021-02-02 23:09:41 +0000

    added dashboard

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -3,3 +3,5 @@ export const DASHBOARD = '/dashboard';
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
 export const FORGOT_PASSWORD = '/forgot-password';
+export const INVENTORY = '/inventory';
+export const SALES = '/sales';
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,12 +1,49 @@
 import React, { useContext } from 'react';
 import { FirebaseContext } from '../../context/firebase';
+import Sidebar from '../../layout/Sidebar';
+import Main from '../../layout/Main';
+import Header from '../../components/Header';
+import Inventory from '../../components/Inventory';
+import Sales from '../../components/Sales';
+import { Switch } from 'react-router-dom';
+import * as ROUTES from '../../constants/routes';
+import { ProtectedRoute } from '../../helpers/routes';
+import { useAuthListener } from '../../hooks';
+import '../../styles/dashboard.scss';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 
-export default function Dashboard() {
+const Dashboard = () => {
   const { firebase } = useContext(FirebaseContext);
+  const { user } = useAuthListener();
   return (
-    <>
-      This is dashboard mate
-      <button onClick={() => firebase.auth().signOut()}>Sign out</button>
-    </>
+    <div className="dashboard__container">
+      <div className="dashboard__container__sidebar">
+        <Sidebar>
+          <div>
+            <Link to="/inventory"> Inventory</Link>
+          </div>
+          <div>
+            <Link to="/sales"> Sales</Link>
+          </div>
+        </Sidebar>
+      </div>
+      <div className="dashboard__container__main">
+        <Header>
+          <button onClick={() => firebase.auth().signOut()}>Sign out</button>
+        </Header>
+        <Main>
+          <Switch>
+            <ProtectedRoute user={user} path={ROUTES.INVENTORY}>
+              <Inventory />
+            </ProtectedRoute>
+            <ProtectedRoute user={user} path={ROUTES.SALES}>
+              <Sales />
+            </ProtectedRoute>
+          </Switch>
+        </Main>
+      </div>
+    </div>
   );
-}
+};
+
+export default Dashboard;
